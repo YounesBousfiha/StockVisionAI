4c6ac3ebcd251d8a2e0842c1984f5a0ddd991c38
Coderyounes <younesanas2021@gmail.com>
2025-12-22 20:01:01 +0100

    FEAT: add Product entity with validation and MongoDB annotations

---
+++ src/main/java/com/jartiste/stockvisionai/domain/entity/Product.java
@@ -0,0 +1,51 @@
+package com.jartiste.stockvisionai.domain.entity;
+
+
+import com.jartiste.stockvisionai.domain.enums.Unite;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Document
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    private UUID uuid;
+
+    @NotBlank
+    private String nom;
+
+    @NotBlank
+    private String description;
+
+    @NotBlank
+    private String categorie;
+
+    @NotBlank
+    private BigDecimal prixVente;
+
+    @NotBlank
+    private BigDecimal prixAchat;
+
+    @NotBlank
+    private BigDecimal marge;
+
+    @NotBlank
+    private Double poids;
+
+    @NotBlank
+    private Unite unite;
+
+    private LocalDateTime creationAt;
+    private LocalDateTime updateAt;
+}
