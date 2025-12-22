546cd4e7767c3f4e7d97219cca5994a6439e9bfd
Coderyounes <younesanas2021@gmail.com>
2025-12-22 20:08:58 +0100

    FEAT: add HistoriqueVente entity with MongoDB annotations

---
+++ src/main/java/com/jartiste/stockvisionai/domain/entity/HistoriqueVente.java
@@ -0,0 +1,30 @@
+package com.jartiste.stockvisionai.domain.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.DayOfWeek;
+import java.time.Month;
+import java.time.Year;
+import java.util.UUID;
+
+@Document
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class HistoriqueVente {
+
+    @Id
+    private UUID uuid;
+
+    private Integer quantity;
+
+    private DayOfWeek dayOfWeek;
+
+    private Month month;
+
+    private Year year;
+}
