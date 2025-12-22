5d897ed4fb8b273f40bc3ec55e86ffd863352db8
Coderyounes <younesanas2021@gmail.com>
2025-12-22 20:09:08 +0100

    FEAT: add Stock entity with validation and MongoDB annotations

---
+++ src/main/java/com/jartiste/stockvisionai/domain/entity/Stock.java
@@ -0,0 +1,25 @@
+package com.jartiste.stockvisionai.domain.entity;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
+@Document
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Stock {
+
+    @Id
+    private UUID uuid;
+
+    @NotBlank
+    private Integer quantity;
+
+    private Integer seuilAlerte;
+}
