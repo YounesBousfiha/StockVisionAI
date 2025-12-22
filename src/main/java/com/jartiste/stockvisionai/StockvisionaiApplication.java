c369faf9f35c354cd7949be8009ee016444e4d32
Coderyounes <younesanas2021@gmail.com>
2025-12-22 19:34:03 +0100

    FEAT: initial commit

---
+++ src/main/java/com/jartiste/stockvisionai/StockvisionaiApplication.java
@@ -0,0 +1,13 @@
+package com.jartiste.stockvisionai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockvisionaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockvisionaiApplication.class, args);
+	}
+
+}
