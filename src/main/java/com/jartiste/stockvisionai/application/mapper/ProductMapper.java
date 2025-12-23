9f010e98bafb8d886771a7e982acee3139b0637e
Coderyounes <younesanas2021@gmail.com>
2025-12-23 22:32:09 +0100

    feat(mapper): implement ProductMapper for mapping between Product and DTOs

--- src/main/java/com/jartiste/stockvisionai/application/mapper/ProductMapper.java
+++ src/main/java/com/jartiste/stockvisionai/application/mapper/ProductMapper.java
@@ -1,4 +1,26 @@
 package com.jartiste.stockvisionai.application.mapper;
 
+import com.jartiste.stockvisionai.domain.entity.Product;
+import com.jartiste.stockvisionai.presentation.dto.request.ProductRequest;
+import com.jartiste.stockvisionai.presentation.dto.response.ProductResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface ProductMapper {
+
+    ProductResponse toResponse(Product product);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "creationAt", ignore = true)
+    @Mapping(target = "updateAt", ignore = true)
+    Product toEntity(ProductRequest dto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "creationAt", ignore = true)
+    @Mapping(target = "updateAt", ignore = true)
+    void updateProductFromDTO(ProductRequest dto, @MappingTarget Product entity);
 }
+
