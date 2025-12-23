1ce14dd967b5843bb29800ad16179f80ecd6ac2c
Coderyounes <younesanas2021@gmail.com>
2025-12-23 21:05:34 +0100

    feat(repository): add EntrepotRepository interface for CRUD operations

---
+++ src/main/java/com/jartiste/stockvisionai/domain/repository/EntrepotRepository.java
@@ -0,0 +1,13 @@
+package com.jartiste.stockvisionai.domain.repository;
+
+import com.jartiste.stockvisionai.domain.entity.Entrepot;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EntrepotRepository {
+    Entrepot save(Entrepot entrepot);
+    Optional<Entrepot> findById(String id);
+    List<Entrepot> findAll();
+    void deleteById(String id);
+}
