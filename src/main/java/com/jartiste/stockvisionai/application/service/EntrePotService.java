3dd66f5a5ff4849d6005a1c66fd1f985c4ec2c6a
Coderyounes <younesanas2021@gmail.com>
2025-12-23 21:58:38 +0100

    feat(service): update EntrePotService interface to include request parameter in update method

--- src/main/java/com/jartiste/stockvisionai/application/service/EntrePotService.java
+++ src/main/java/com/jartiste/stockvisionai/application/service/EntrePotService.java
@@ -1,15 +1,14 @@
 package com.jartiste.stockvisionai.application.service;
 
-import com.jartiste.stockvisionai.domain.entity.Entrepot;
 import com.jartiste.stockvisionai.presentation.dto.request.EntrePotRequest;
 import com.jartiste.stockvisionai.presentation.dto.response.EntrePotResponse;
 
 import java.util.List;
 
 public interface EntrePotService {
-    public EntrePotResponse createEntrepot(EntrePotRequest request);
-    public List<EntrePotResponse> getAllEntrepot();
-    public EntrePotResponse findOneEntrepot(String id);
-    public EntrePotResponse updateEntrepot(String id);
-    public void deleteEntrepot(String id);
+    EntrePotResponse createEntrepot(EntrePotRequest request);
+    List<EntrePotResponse> getAllEntrepot();
+    EntrePotResponse findOneEntrepot(String id);
+    EntrePotResponse updateEntrepot(String id, EntrePotRequest request);
+    void deleteEntrepot(String id);
 }
