0b4c63b3a1ced013a228a300c14dc6606f74c4e7
MohamedMoustir <itsmoustir@gmail.com>
2025-12-23 14:10:36 +0100

    refactor: update custom user details service

--- src/main/java/com/jartiste/stockvisionai/infrastructure/service/CustomUserDetailsService.java
+++ src/main/java/com/jartiste/stockvisionai/infrastructure/service/CustomUserDetailsService.java
@@ -1,4 +1,29 @@
 package com.jartiste.stockvisionai.infrastructure.service;
 
-public class CustomUserDetailsService {
+
+import com.jartiste.stockvisionai.domain.entity.User;
+import com.jartiste.stockvisionai.domain.repository.UserRepository;
+import com.jartiste.stockvisionai.infrastructure.service.AutheticatedUser;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return new AutheticatedUser(user);
+    }
 }
+
