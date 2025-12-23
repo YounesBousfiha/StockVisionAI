cdc9e428f3bcc58c8217c394e16bad84baf6ea80
Coderyounes <younesanas2021@gmail.com>
2025-12-23 22:26:45 +0100

    feat(repository): implement ProductRepositoryImpl for product data operations

---
+++ src/main/java/com/jartiste/stockvisionai/infrastructure/persistence/ProductRepositoryImpl.java
@@ -0,0 +1,31 @@
+package com.jartiste.stockvisionai.infrastructure.persistence;
+
+import com.jartiste.stockvisionai.domain.entity.Product;
+import com.jartiste.stockvisionai.domain.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class ProductRepositoryImpl implements ProductRepository {
+
+    private final SpringDataProductRepository repo;
+
+    @Override
+    public Product save(Product p) {
+        return repo.save(p);
+    }
+
+    @Override
+    public Optional<Product> findById(String id) {
+        return repo.findById(id);
+    }
+
+    @Override
+    public List<Product> findAllById(Iterable<String> ids) {
+        return repo.findAllById(ids);
+    }
+}
