0050a4a87310dabc4babea6c8243df86b27d04e2
Coderyounes <younesanas2021@gmail.com>
2025-12-23 22:26:08 +0100

    feat(repository): implement StockRepository interface in StockRepositoryImpl

---
+++ src/main/java/com/jartiste/stockvisionai/infrastructure/persistence/StockRepositoryImpl.java
@@ -0,0 +1,46 @@
+package com.jartiste.stockvisionai.infrastructure.persistence;
+
+import com.jartiste.stockvisionai.domain.entity.Stock;
+import com.jartiste.stockvisionai.domain.repository.StockRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class StockRepositoryImpl implements StockRepository {
+
+    private final SpringDataStockRepository repo;
+
+    @Override
+    public Stock save(Stock s) {
+        return repo.save(s);
+    }
+
+    @Override
+    public Optional<Stock> findById(String id) {
+        return repo.findById(id);
+    }
+
+    @Override
+    public List<Stock> findByEntrepotId(String entrepotId) {
+        return repo.findByEntrepotId(entrepotId);
+    }
+
+    @Override
+    public List<Stock> findByProductIdIn(List<String> productIds) {
+        return repo.findByProductIdIn(productIds);
+    }
+
+    @Override
+    public List<Stock> findAll() {
+        return repo.findAll();
+    }
+
+    @Override
+    public void deleteById(String id) {
+        repo.deleteById(id);
+    }
+}
