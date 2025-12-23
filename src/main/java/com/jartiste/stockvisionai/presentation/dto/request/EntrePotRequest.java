bc2de740c154b8f6d6ed3a5f9b1c18f864036006
Coderyounes <younesanas2021@gmail.com>
2025-12-23 21:05:19 +0100

    feat(dto): add EntrePotRequest DTO with validation annotations

---
+++ src/main/java/com/jartiste/stockvisionai/presentation/dto/request/EntrePotRequest.java
@@ -0,0 +1,15 @@
+package com.jartiste.stockvisionai.presentation.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+
+@Builder
+public record EntrePotRequest(
+        @NotBlank
+        String nom,
+        @NotBlank
+        String address,
+        @NotBlank
+        String ville
+) {
+}
