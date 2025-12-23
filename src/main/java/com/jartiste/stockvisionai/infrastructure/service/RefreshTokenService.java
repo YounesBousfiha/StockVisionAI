94ccc1eef66e55802e803557419233c09fd62324
MohamedMoustir <itsmoustir@gmail.com>
2025-12-23 16:59:15 +0100

    refactor(model): update Compte and Operation inheritance structure

---
+++ src/main/java/com/jartiste/stockvisionai/infrastructure/service/RefreshTokenService.java
@@ -0,0 +1,4 @@
+package com.jartiste.stockvisionai.infrastructure.service;
+
+public class RefreshTokenService {
+}
---
+++ src/main/java/com/jartiste/stockvisionai/presentation/dto/response/UserResponseDTO.java
@@ -0,0 +1,4 @@
+package com.jartiste.stockvisionai.presentation.dto.response;
+
+public class UserResponseDTO {
+}
