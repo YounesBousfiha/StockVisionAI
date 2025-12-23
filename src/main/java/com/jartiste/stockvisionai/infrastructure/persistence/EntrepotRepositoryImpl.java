e734bb3e52520a4158ccbe130cf74990a608a2d1
Coderyounes <younesanas2021@gmail.com>
2025-12-23 21:05:39 +0100

    feat(repository): implement EntrepotRepository interface with CRUD operations

---
+++ src/main/java/com/jartiste/stockvisionai/infrastructure/persistence/EntrepotRepositoryImpl.java
@@ -0,0 +1,39 @@
+package com.jartiste.stockvisionai.infrastructure.persistence;
+
+import com.jartiste.stockvisionai.domain.entity.Entrepot;
+import com.jartiste.stockvisionai.domain.repository.EntrepotRepository;
+import com.jartiste.stockvisionai.domain.repository.ProducRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Component
+@RequiredArgsConstructor
+public class EntrepotRepositoryImpl  implements EntrepotRepository {
+
+    private final SpringDataEntrepotRepository jpaRepository;
+
+    @Override
+    public Entrepot save(Entrepot entrepot) {
+        return jpaRepository.save(entrepot);
+    }
+
+    @Override
+    public Optional<Entrepot> findById(String id) {
+        return jpaRepository.findById(id);
+    }
+
+    @Override
+    public List<Entrepot> findAll() {
+        return jpaRepository.findAll();
+    }
+
+    @Override
+    public void deleteById(String id) {
+        jpaRepository.deleteById(id);
+    }
+}
