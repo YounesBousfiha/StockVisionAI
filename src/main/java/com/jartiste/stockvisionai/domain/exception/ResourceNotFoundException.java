cd4776a41e4a320b5069dbe1a011a8d895df277b
Coderyounes <younesanas2021@gmail.com>
2025-12-23 21:58:55 +0100

    feat(exception): create ResourceNotFoundException class

---
+++ src/main/java/com/jartiste/stockvisionai/domain/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.jartiste.stockvisionai.domain.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
+
