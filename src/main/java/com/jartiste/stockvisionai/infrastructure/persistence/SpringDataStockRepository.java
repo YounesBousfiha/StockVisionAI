e694b602b671402e927d5c76e09dacf7c6f435b7
Coderyounes <younesanas2021@gmail.com>
2025-12-23 22:25:59 +0100

    feat(repository): add SpringDataStockRepository for Stock entity persistence

---
+++ src/main/java/com/jartiste/stockvisionai/infrastructure/persistence/SpringDataStockRepository.java
@@ -0,0 +1,13 @@
+package com.jartiste.stockvisionai.infrastructure.persistence;
+
+import com.jartiste.stockvisionai.domain.entity.Stock;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SpringDataStockRepository extends MongoRepository<Stock, String> {
+    List<Stock> findByEntrepotId(String entrepotId);
+    List<Stock> findByProductIdIn(List<String> productIds);
+}
