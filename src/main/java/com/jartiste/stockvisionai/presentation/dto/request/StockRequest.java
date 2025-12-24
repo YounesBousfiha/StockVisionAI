16a0105f17462fac03616fa36c6b87b73fb3a0db
Coderyounes <younesanas2021@gmail.com>
2025-12-24 23:54:55 +0100

    feat(model): update StockRequest record to use Lombok's Builder and simplify fields

--- src/main/java/com/jartiste/stockvisionai/presentation/dto/request/StockRequest.java
+++ src/main/java/com/jartiste/stockvisionai/presentation/dto/request/StockRequest.java
@@ -1,17 +1,14 @@
 package com.jartiste.stockvisionai.presentation.dto.request;
 
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 
+@Builder
 public record StockRequest(
         @NotNull
         @Min(0)
         Integer quantity,
-        Integer seuilAlerte,
-        @NotBlank
-        String productId,
-        @NotBlank
-        String entrepotId
+        Integer seuilAlerte
 ) {}
 
