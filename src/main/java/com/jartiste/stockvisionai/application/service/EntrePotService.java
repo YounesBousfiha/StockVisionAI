e92ccbd20c798ccac695b75441f8056f50502007
Coderyounes <younesanas2021@gmail.com>
2025-12-24 21:07:10 +0100

    feat(service): update updateEntrepot method to use EntrepotUpdateRequest for validation

--- src/main/java/com/jartiste/stockvisionai/application/service/EntrePotService.java
+++ src/main/java/com/jartiste/stockvisionai/application/service/EntrePotService.java
@@ -1,7 +1,9 @@
 package com.jartiste.stockvisionai.application.service;
 
 import com.jartiste.stockvisionai.presentation.dto.request.EntrePotRequest;
+import com.jartiste.stockvisionai.presentation.dto.request.EntrepotUpdateRequest;
 import com.jartiste.stockvisionai.presentation.dto.response.EntrePotResponse;
+import jakarta.validation.Valid;
 
 import java.util.List;
 
@@ -9,6 +11,6 @@ public interface EntrePotService {
     EntrePotResponse createEntrepot(EntrePotRequest request);
     List<EntrePotResponse> getAllEntrepot();
     EntrePotResponse findOneEntrepot(String id);
-    EntrePotResponse updateEntrepot(String id, EntrePotRequest request);
+    EntrePotResponse updateEntrepot(String id, @Valid EntrepotUpdateRequest request);
     void deleteEntrepot(String id);
 }
