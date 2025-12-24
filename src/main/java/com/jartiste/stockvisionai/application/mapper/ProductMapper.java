407fd2c6d9c76608120fd795de703cf1c6395b62
Coderyounes <younesanas2021@gmail.com>
2025-12-24 23:54:01 +0100

    feat(mapper): add mapping method for updating Product from ProductUpdateRequest

--- src/main/java/com/jartiste/stockvisionai/application/mapper/ProductMapper.java
+++ src/main/java/com/jartiste/stockvisionai/application/mapper/ProductMapper.java
@@ -2,6 +2,7 @@ package com.jartiste.stockvisionai.application.mapper;
 
 import com.jartiste.stockvisionai.domain.entity.Product;
 import com.jartiste.stockvisionai.presentation.dto.request.ProductRequest;
+import com.jartiste.stockvisionai.presentation.dto.request.ProductUpdateRequest;
 import com.jartiste.stockvisionai.presentation.dto.response.ProductResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -22,5 +23,10 @@ public interface ProductMapper {
     @Mapping(target = "creationAt", ignore = true)
     @Mapping(target = "updateAt", ignore = true)
     void updateProductFromDTO(ProductRequest dto, @MappingTarget Product entity);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "creationAt", ignore = true)
+    @Mapping(target = "updateAt", ignore = true)
+    void updateProductFromUpdateDTO(ProductUpdateRequest dto, @MappingTarget Product entity);
 }
 
