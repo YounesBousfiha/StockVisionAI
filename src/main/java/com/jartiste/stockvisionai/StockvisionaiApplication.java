9fb3044ecb0ebc87a86c413ae41d40ff5365d81a
Coderyounes <younesanas2021@gmail.com>
2025-12-24 21:08:26 +0100

    feat(application): implement CommandLineRunner to log JWT secret key on startup

--- src/main/java/com/jartiste/stockvisionai/StockvisionaiApplication.java
+++ src/main/java/com/jartiste/stockvisionai/StockvisionaiApplication.java
@@ -1,13 +1,27 @@
 package com.jartiste.stockvisionai;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StockvisionaiApplication {
+@Slf4j
+public class StockvisionaiApplication implements CommandLineRunner {
 
+
+	@Value( "${jwt.secret-key}")
+	private String jwtSecret;
 	public static void main(String[] args) {
 		SpringApplication.run(StockvisionaiApplication.class, args);
+
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("JWT Secret : {}", jwtSecret);
+	}
+
 
 }
