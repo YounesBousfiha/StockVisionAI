eb55a23af78537b98b32993cdf3a2aa252f3c2cf
Coderyounes <younesanas2021@gmail.com>
2025-12-24 23:55:08 +0100

    feat(service): update StockService interface with CRUD methods for stock management

--- src/main/java/com/jartiste/stockvisionai/application/service/StockService.java
+++ src/main/java/com/jartiste/stockvisionai/application/service/StockService.java
@@ -1,12 +1,16 @@
 package com.jartiste.stockvisionai.application.service;
 
 import com.jartiste.stockvisionai.presentation.dto.request.StockRequest;
+import com.jartiste.stockvisionai.presentation.dto.request.StockUpdateRequest;
 import com.jartiste.stockvisionai.presentation.dto.response.StockResponse;
 
 import java.util.List;
 
 public interface StockService {
-    StockResponse createStock(StockRequest request);
+    StockResponse createStock(String entrepotId, String productId, StockRequest request);
     List<StockResponse> findByEntrepotId(String entrepotId);
+    List<StockResponse> findAllStocks();
+    StockResponse findStockById(String id);
+    StockResponse updateStock(String id, StockUpdateRequest request);
+    void deleteStock(String id);
 }
-
