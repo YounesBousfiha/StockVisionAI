0d0739934b98d69632f189180f16b547a88d43d5
Coderyounes <younesanas2021@gmail.com>
2025-12-26 00:21:47 +0100

    feat(historique-vente): add repository interface for HistoriqueVente entity

---
+++ src/main/java/com/jartiste/stockvisionai/domain/repository/HistoriqueVenteRepository.java
@@ -0,0 +1,10 @@
+package com.jartiste.stockvisionai.domain.repository;
+import com.jartiste.stockvisionai.domain.entity.HistoriqueVente;
+import java.util.List;
+import java.util.Optional;
+public interface HistoriqueVenteRepository {
+    HistoriqueVente save(HistoriqueVente historiqueVente);
+    Optional<HistoriqueVente> findById(String id);
+    List<HistoriqueVente> findAll();
+    void deleteById(String id);
+}
