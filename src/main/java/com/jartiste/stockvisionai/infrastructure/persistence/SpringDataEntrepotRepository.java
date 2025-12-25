ba1d94552d082351ccb2ec78a727187cbbad1081
Coderyounes <younesanas2021@gmail.com>
2025-12-25 22:13:41 +0100

    feat(entrepot): add method to find entrepots by gestionnaireId

--- src/main/java/com/jartiste/stockvisionai/infrastructure/persistence/SpringDataEntrepotRepository.java
+++ src/main/java/com/jartiste/stockvisionai/infrastructure/persistence/SpringDataEntrepotRepository.java
@@ -4,6 +4,9 @@ import com.jartiste.stockvisionai.domain.entity.Entrepot;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SpringDataEntrepotRepository extends MongoRepository<Entrepot, String> {
+    List<Entrepot> findByGestionnaireId(String gestionnaireId);
 }
