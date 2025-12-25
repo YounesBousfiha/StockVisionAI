a12fd8da7720b49a4c1a6e9a52d56f563542544f
Coderyounes <younesanas2021@gmail.com>
2025-12-25 22:13:32 +0100

    feat(entrepot): implement assign and deassign methods for gestionnaires

--- src/main/java/com/jartiste/stockvisionai/application/service/implementation/EntrepotServiceImpl.java
+++ src/main/java/com/jartiste/stockvisionai/application/service/implementation/EntrepotServiceImpl.java
@@ -4,6 +4,8 @@ package com.jartiste.stockvisionai.application.service.implementation;
 import com.jartiste.stockvisionai.application.mapper.EntrepotMapper;
 import com.jartiste.stockvisionai.application.service.EntrePotService;
 import com.jartiste.stockvisionai.domain.entity.Entrepot;
+import com.jartiste.stockvisionai.domain.exception.AlreadyAssignedException;
+import com.jartiste.stockvisionai.domain.exception.NonAssignedEntrepot;
 import com.jartiste.stockvisionai.domain.exception.ResourceNotFoundException;
 import com.jartiste.stockvisionai.domain.repository.EntrepotRepository;
 import com.jartiste.stockvisionai.presentation.dto.request.EntrePotRequest;
@@ -51,6 +53,13 @@ public class EntrepotServiceImpl implements EntrePotService {
     }
 
     @Override
+    public List<EntrePotResponse> findByGestionnaireId(String gestionnaireId) {
+        return entrepotRepository.findByGestionnaireId(gestionnaireId).stream()
+                .map(entrepotMapper::toResponse)
+                .toList();
+    }
+
+    @Override
     public EntrePotResponse updateEntrepot(String id, @Valid EntrepotUpdateRequest request) {
         Entrepot entity = entrepotRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException(NOT_FOUND_MSG + id));
@@ -66,6 +75,44 @@ public class EntrepotServiceImpl implements EntrePotService {
         entrepotRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException(NOT_FOUND_MSG + id));
         entrepotRepository.deleteById(id);
+    }
+
+    @Override
+    public EntrePotResponse assign(String entrepotId, String gestionnaireId) {
+        Entrepot entrepot = this.entrepotRepository.findById(entrepotId)
+                .orElseThrow(() -> new ResourceNotFoundException(NOT_FOUND_MSG + entrepotId));
+
+        List<Entrepot> entrepots = entrepotRepository.findByGestionnaireId(gestionnaireId);
+
+        if(!entrepots.isEmpty()) {
+            throw new AlreadyAssignedException("This User is Already Assigned to another Entrepot");
+        }
+
+        if(entrepot.getGestionnaireId() != null) {
+            throw new AlreadyAssignedException("This Entrepot is Already Assigned");
+        }
+
+        entrepot.setGestionnaireId(gestionnaireId);
+        entrepot.setUpdateAt(LocalDateTime.now());
+
+        return entrepotMapper.toResponse(entrepotRepository.save(entrepot));
+    }
+
+    @Override
+    public EntrePotResponse deassign(String entrepotId) {
+        Entrepot entrepot = this.entrepotRepository.findById(entrepotId)
+                .orElseThrow(() -> new ResourceNotFoundException(NOT_FOUND_MSG + entrepotId));
+
+        if(entrepot.getGestionnaireId() == null) {
+            throw new NonAssignedEntrepot("This Entrepot is Already Unassigned");
+        }
+
+        entrepot.setGestionnaireId(null);
+
+        entrepot.setUpdateAt(LocalDateTime.now());
+
+        return entrepotMapper.toResponse(entrepotRepository.save(entrepot));
+
     }
 
 }
