272b87a320c99aa91f2de75c6199a57b0e1a0a74
Coderyounes <younesanas2021@gmail.com>
2025-12-25 15:25:32 +0100

    feat(response): enhance ProductResponse with JSON serialization controls for admin access

--- src/main/java/com/jartiste/stockvisionai/presentation/dto/response/ProductResponse.java
+++ src/main/java/com/jartiste/stockvisionai/presentation/dto/response/ProductResponse.java
@@ -1,10 +1,13 @@
 package com.jartiste.stockvisionai.presentation.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.jartiste.stockvisionai.domain.enums.Unite;
+import com.jartiste.stockvisionai.infrastructure.security.json.AdminAccessFilter;
 import lombok.Builder;
 
 import java.math.BigDecimal;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Builder
 public record ProductResponse(
         String id,
@@ -12,7 +15,11 @@ public record ProductResponse(
         String description,
         String category,
         BigDecimal prixVente,
+
+        @JsonInclude(value = JsonInclude.Include.CUSTOM, valueFilter = AdminAccessFilter.class)
         BigDecimal prixAchat,
+
+        @JsonInclude(value = JsonInclude.Include.CUSTOM, valueFilter = AdminAccessFilter.class)
         BigDecimal marge,
         Double poids,
         Unite unite,
