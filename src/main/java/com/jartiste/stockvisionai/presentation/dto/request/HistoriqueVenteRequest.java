ec13b8afaf8dea403b91717d2d2a4cefcdf4b7bc
Coderyounes <younesanas2021@gmail.com>
2025-12-26 00:22:01 +0100

    feat(historique-vente): add DTO for HistoriqueVente request validation

---
+++ src/main/java/com/jartiste/stockvisionai/presentation/dto/request/HistoriqueVenteRequest.java
@@ -0,0 +1,10 @@
+package com.jartiste.stockvisionai.presentation.dto.request;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+@Builder
+public record HistoriqueVenteRequest(
+        @NotNull
+        @Min(1)
+        Integer quantity
+) {}
