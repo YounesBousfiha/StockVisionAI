62f1c6ec78ad62bdb1226382e2be6a9583a3e12c
MohamedMoustir <itsmoustir@gmail.com>
2025-12-26 21:30:17 +0100

    feat: enhance stock repository to support AI data retrieval

--- src/main/java/com/jartiste/stockvisionai/infrastructure/persistence/SpringDataStockRepository.java
+++ src/main/java/com/jartiste/stockvisionai/infrastructure/persistence/SpringDataStockRepository.java
@@ -1,5 +1,6 @@
 package com.jartiste.stockvisionai.infrastructure.persistence;
 
+import com.jartiste.stockvisionai.domain.entity.HistoriqueVente;
 import com.jartiste.stockvisionai.domain.entity.Stock;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,6 @@ import java.util.List;
 public interface SpringDataStockRepository extends MongoRepository<Stock, String> {
     List<Stock> findByEntrepotId(String entrepotId);
     List<Stock> findByProductIdIn(List<String> productIds);
+    Stock findByEntrepotIdAndProductId(String entrepotId, String productId);
+
 }
