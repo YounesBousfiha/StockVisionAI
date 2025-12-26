acaa6091862ff725799c200e7e093411bbef6688
MohamedMoustir <itsmoustir@gmail.com>
2025-12-26 22:31:17 +0100

    feat: implement CRUD business logic for prediction management

--- src/main/java/com/jartiste/stockvisionai/application/service/PrevisionService.java
+++ src/main/java/com/jartiste/stockvisionai/application/service/PrevisionService.java
@@ -1,4 +1,12 @@
 package com.jartiste.stockvisionai.application.service;
 
-public class PrevisionService {
+
+import com.jartiste.stockvisionai.presentation.dto.request.PrevisionRequest;
+import com.jartiste.stockvisionai.presentation.dto.response.PrevisionResponse;
+
+import java.util.List;
+
+public interface PrevisionService {
+    PrevisionResponse createPrevision(PrevisionRequest request);
+    List<PrevisionResponse> findAllPrevision();
 }
