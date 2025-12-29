2b088468b72f379868a1d7fc429553369e024c3f
Coderyounes <younesanas2021@gmail.com>
2025-12-30 00:29:30 +0100

    feat(entrepot): remove gestionnaireId field from EntrepotUpdateRequest

--- src/main/java/com/jartiste/stockvisionai/presentation/dto/request/EntrepotUpdateRequest.java
+++ src/main/java/com/jartiste/stockvisionai/presentation/dto/request/EntrepotUpdateRequest.java
@@ -3,7 +3,6 @@ package com.jartiste.stockvisionai.presentation.dto.request;
 public record EntrepotUpdateRequest(
         String nom,
         String address,
-        String ville,
-        String gestionnaireId
+        String ville
 ) {
 }
