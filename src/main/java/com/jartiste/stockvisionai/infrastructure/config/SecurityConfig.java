733562b7732ae2220dae34aaa5c5cf64f0e3af27
Coderyounes <younesanas2021@gmail.com>
2025-12-30 14:57:18 +0100

    feat(security): add profile annotation to AutheticatedUser and SecurityConfig for non-production environments

--- src/main/java/com/jartiste/stockvisionai/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/jartiste/stockvisionai/infrastructure/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.jartiste.stockvisionai.infrastructure.service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -20,6 +21,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@Profile("!prod")
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
