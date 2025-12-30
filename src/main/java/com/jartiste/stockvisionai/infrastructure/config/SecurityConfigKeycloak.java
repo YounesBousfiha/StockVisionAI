5fd5a9ecd21549fedf16b62cf44d0834ba34b079
Coderyounes <younesanas2021@gmail.com>
2025-12-30 14:56:12 +0100

    feat(security): add Keycloak security configuration for production environment

---
+++ src/main/java/com/jartiste/stockvisionai/infrastructure/config/SecurityConfigKeycloak.java
@@ -0,0 +1,51 @@
+package com.jartiste.stockvisionai.infrastructure.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@Profile("prod")
+public class SecurityConfigKeycloak {
+
+    private final KeycloakAuthConverter keycloakAuthConverter;
+
+    public SecurityConfigKeycloak(KeycloakAuthConverter keycloakAuthConverter) {
+        this.keycloakAuthConverter = keycloakAuthConverter;
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(keycloakAuthConverter)));
+
+
+                return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
